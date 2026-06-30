725d9fc9069f50e49a58ad7158c6207786d7cf1d
mrherocop <herocop3.0@gmail.com>
2026-06-30 15:26:24 +0530

    Fix: Remove ?attr/colorControlNormal from ic_volume_outline.xml which crashes RemoteViews and Notifications with Resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.volumeq.app"
         minSdk = 29
         targetSdk = 35
-        versionCode = 3
-        versionName = "3.1"
+        versionCode = 4
+        versionName = "3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
